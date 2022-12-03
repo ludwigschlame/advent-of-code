223d0402f309a926a436a5b234019c8dcfeddddb
Ludwig Schlame <ludwig.schlame@gmail.com>
2022-12-03 19:55:15 +0100

    AoC-2022-day03: some small generalizations

--- 2022/day03/src/main.rs
+++ 2022/day03/src/main.rs
@@ -2,8 +2,8 @@ use std::fs;
 
 fn item_priority(item: char) -> u8 {
     match item as u8 {
-        65..=90 => item as u8 - 64 + 26, /* uppercase letters */
-        97..=122 => item as u8 - 96,     /* lowercase letters */
+        b'a'..=b'z' => item as u8 - b'a' + 1,  /* lowercase letters */
+        b'A'..=b'Z' => item as u8 - b'A' + 27, /* uppercase letters */
         _ => panic!("Error: invalid item"),
     }
 }
@@ -15,10 +15,10 @@ fn main() {
         fs::read_to_string(path).unwrap_or_else(|err| panic!("Error reading '{}': {}", path, err));
 
     /* SHARED */
-    let mut lines = input.trim().lines().map(|line| line.trim());
+    let mut lines = input.trim().lines();
 
     /* PART ONE */
-    let mut priority_sum: u32 = 0;
+    let mut priority_sum: usize = 0;
 
     for rucksack in lines.clone() {
         let compartment_size = rucksack.len() / 2;
@@ -26,7 +26,7 @@ fn main() {
 
         for item in rucksack_compartments.0.chars() {
             if rucksack_compartments.1.find(item).is_some() {
-                priority_sum += item_priority(item) as u32;
+                priority_sum += item_priority(item) as usize;
                 break;
             }
         }
@@ -34,14 +34,14 @@ fn main() {
 
     assert_eq!(priority_sum, 8349); /* verified result */
     println!(
-        "PART ONE: {} is the sum of the incorrectly sorted items' priorities",
+        "PART ONE: {} is the sum of the incorrectly sorted items' priorities.",
         priority_sum
     );
 
     /* PART TWO */
-    let mut priority_sum = 0;
+    let mut priority_sum: usize = 0;
 
-    for _ in 0..lines.clone().count() {
+    for _ in 0..lines.clone().count() / 3 {
         let elf_group;
         if let (Some(elf1), Some(elf2), Some(elf3)) = (lines.next(), lines.next(), lines.next()) {
             elf_group = (elf1, elf2, elf3);
@@ -51,7 +51,7 @@ fn main() {
 
         for item in elf_group.0.chars() {
             if elf_group.1.find(item).is_some() && elf_group.2.find(item).is_some() {
-                priority_sum += item_priority(item) as u32;
+                priority_sum += item_priority(item) as usize;
                 break;
             }
         }
@@ -59,7 +59,7 @@ fn main() {
 
     assert_eq!(priority_sum, 2681); /* verified result */
     println!(
-        "PART TWO: {} is the sum of identification badges' priorities",
+        "PART TWO: {} is the sum of identification badges' priorities.",
         priority_sum
     );
 }
